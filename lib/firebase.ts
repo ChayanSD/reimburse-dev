d0381165531fdcaa5cbc8a907ecd6558619383cd
reimbursemeai <reimbursemeai@gmail.com>
2026-01-22 08:54:52 -0600

    Firebase Config Fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP199eOv-Cf3EWlCdGTKZU0c4tRmCV9UU",
-  authDomain: "reimburseme-a78c8.firebaseapp.com",
-  projectId: "reimburseme-a78c8",
-  storageBucket: "reimburseme-a78c8.firebasestorage.app",
-  messagingSenderId: "789764423926",
-  appId: "1:789764423926:web:076af2d3dff631ee602cd7",
-  measurementId: "G-9V708FBHZS"
+  apiKey: "AIzaSyCwU3uqQKjnE3o4X_gtS71UjQHvqXUz9Zg",
+  authDomain: "reimburseme-2eb3c.firebaseapp.com",
+  projectId: "reimburseme-2eb3c",
+  storageBucket: "reimburseme-2eb3c.firebasestorage.app",
+  messagingSenderId: "531228568328",
+  appId: "1:531228568328:web:cd377e46236e725c379abd",
+  measurementId: "G-KV8DVZ6S8R"
 };
 
 // Initialize Firebase
